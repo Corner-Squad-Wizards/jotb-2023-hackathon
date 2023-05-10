b8182bf1a96a4215163c11a872e305f491e882ca
david-zumaquero <97678893+david-zumaquero@users.noreply.github.com>
2023-05-10 17:32:47 +0200

    refactor: Useless files

--- src/pages/4_📊_Statistics.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import streamlit as st
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
-
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with"""
-)
