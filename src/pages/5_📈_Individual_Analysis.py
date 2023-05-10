47ab726610ba14171a369c0728fca30d6a10f305
Carlos Fernandez <carlos_alejandro_fernandez_lemus@external.mckinsey.com>
2023-05-10 16:11:55 +0200

    add metrics for loans

--- src/pages/5_📈_Individual_Analysis.py
+++ src/pages/5_📈_Individual_Analysis.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 st.set_page_config(page_title="Plotting Demo", page_icon="📈")
 
 st.markdown("# Mapping Demo")
@@ -7,4 +7,9 @@ st.sidebar.header("Mapping Demo")
 st.write(
     """This demo shows how to use
 [`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-to display geospatial data.""")
+to display geospatial data.
+""")
+
+
+df = pd.read_csv("data/loan_data_shortened.csv")
+st.bar_chart(data=df.head(20),x=["id"], y=["accepted"], use_container_width=True)
