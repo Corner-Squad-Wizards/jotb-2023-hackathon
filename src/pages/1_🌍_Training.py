e62cf0f7dea9e161d42a5ec3fa65e0ded9d23b91
david-zumaquero <97678893+david-zumaquero@users.noreply.github.com>
2023-05-10 17:08:43 +0200

    feat: Correlation matrix

--- src/pages/1_🌍_Training.py
+++ src/pages/1_🌍_Training.py
@@ -2,6 +2,9 @@ from io import StringIO
 
 import streamlit as st
 import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 from urllib.error import URLError
 
 st.set_page_config(page_title="Inference", page_icon="👟")
@@ -9,20 +12,28 @@ st.set_page_config(page_title="Inference", page_icon="👟")
 st.markdown("# Inference")
 st.sidebar.header("Inference")
 
-"""Please provide us data which fit: """
+"""Please provide us data in csv format with columns: """
 
-"""-"""
+"""- debt_to_income"""
 
-"""-"""
+"""- loan_amount"""
 
-"""-"""
+"""- employment_length"""
+
+
+"""- risk_score"""
+"""- acepted (target variable)"""
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file:
     dataframe = pd.read_csv(uploaded_file)
 
-    """ Data Exploratory 🔎"""
+    """ ## Data Exploratory 🔎"""
     st.write(dataframe.head())
+    """ ## Correlation matrix"""
+    fig, ax = plt.subplots()
+    sns.heatmap(dataframe.corr(), ax=ax)
+    st.write(fig)
 
     if st.button("Train model"):
         # TODO: add train function
--- src/pages/4_📊_Statistics.py
+++ src/pages/4_📊_Statistics.py
@@ -7,6 +7,3 @@ st.sidebar.header("Plotting Demo")
 st.write(
     """This demo illustrates a combination of plotting and animation with"""
 )
-
-df = pd.read_csv("data/loan_data_shortened.csv")
-st.bar_chart(data=df.head(10), x=["id"], y=["accepted"], use_container_width=True)
